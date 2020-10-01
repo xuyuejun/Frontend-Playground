7c35054d8c2e40137dacb42a3ddf7522ec81b498
xuyuejun <lwxuyuejun@163.com>
2020-10-01 14:25:55 +0900

    Array

---
+++ example/algorithm/01-Array.js
@@ -0,0 +1,7 @@
+let daysOfWeek1 = new Array(); // {1}
+daysOfWeek2 = new Array(7); // {2}
+daysOfWeek3 = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'); // {3}
+
+console.log(daysOfWeek1)
+console.log(daysOfWeek2)
+console.log(daysOfWeek3)
