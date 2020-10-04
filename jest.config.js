fe06975bb596b4a64a39b7abe56927734569c4c3
xuyuejun <lwxuyuejun@163.com>
2020-10-04 22:40:04 +0900

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+//   clearMocks: true,
+//   coverageDirectory: "coverage",
+//   coverageProvider: "v8",
+  testEnvironment: "node",
+  testMatch: [
+    "**/tests/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+};
---
+++ tests/unit/hello-test.js
@@ -0,0 +1,4 @@
+const add = require('../../src/jest/hello.js');
+test('two plus two is four', () => {
+    expect(2 + 2).toBe(4);
+});
